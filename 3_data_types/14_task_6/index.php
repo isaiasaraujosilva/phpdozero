77a36a95d97f2aa9fab8c5f3e375978e8c19f384
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-12 20:30:38 -0300

    training associative array

---
+++ 3_data_types/14_task_6/index.php
@@ -0,0 +1,7 @@
+<?php
+    $arr=['modelo'=>'sedan','cor'=>'vermelho','potencia'=>'50 cavalos'];
+    print_r($arr);
+    echo $arr['modelo']."<br>";
+    echo $arr['cor']."<br>";
+    echo $arr['potencia']."<br>";
+?>
