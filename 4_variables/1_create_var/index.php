6311d49f96532cfe337d7c2fe7002ef7a96cb775
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-12 21:54:38 -0300

    understanding variables

---
+++ 4_variables/1_create_var/index.php
@@ -0,0 +1,17 @@
+<?php
+    $str="ABC";
+    $int=10;
+    $boo=true;
+    $_flt=1.1;
+    echo "<br>";
+    echo $str;
+    echo "<br>";
+    echo $int;
+    echo "<br>";
+    echo $boo;
+    echo "<br>";
+    echo $_flt;
+    //o php permite iniciar variaveis com "$_"
+    //o php é sensivel ao case ou seja letras maiusculas e minusculas são diferenciadas
+
+?>
