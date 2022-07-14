9bcbc719cfe2ad6d2b6dce549d419f70116cb966
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-13 22:07:05 -0300

    function with returne

---
+++ 4_variables/9_scopo_par/index.php
@@ -0,0 +1,13 @@
+<?php
+    function soma($a,$b){
+        $s=$a+$b;
+        return $s;
+    }
+
+    echo "<br>";
+    echo soma(1,1);
+    echo "<br>";
+    echo soma(1,2);
+    echo "<br>";
+    echo soma(1,3);
+?>
