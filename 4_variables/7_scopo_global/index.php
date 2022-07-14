e1463d5fd231fc46f1aacf9f51a1b6cdb17b393b
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-13 22:05:58 -0300

    test var global

--- 4_variables/7_scopo_global/index.php
+++ 4_variables/7_scopo_global/index.php
@@ -6,8 +6,17 @@
     //Variaveis dentro do if estão no escopo global
     if(true){
         $teste="sba";
+        echo " Alterando a variavel dentro de um bloco if";
     }
 
     echo "<br>";
     echo $teste;
+    //função global
+
+    function testeGlobal(){
+        global $teste;
+        echo " variavel dentro da função --$teste--";
+    }
+
+    testeGlobal();
 ?>
