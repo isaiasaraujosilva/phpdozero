e3909246b91e417f52fa8637e8682afdbb645757
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-15 19:50:26 -0300

    function gettyp and learning

---
+++ 5_op_expres/4_task_11/index.php
@@ -0,0 +1,14 @@
+<?php
+    $r="5"*12;
+
+    echo $r."<br>";
+
+    echo gettype($r)."<br>";
+
+    echo gettype("teste")."<br>";
+
+    echo gettype(12.2)."<br>";
+
+    echo gettype(true)."<br>";
+
+?>
