883fb21abc7b28fb589ba2be8657729e06c973cc
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-15 22:30:03 -0300

    increase and decrease

---
+++ 5_op_expres/12_auto_incr_auto_decr/index.php
@@ -0,0 +1,19 @@
+<?php
+    $a = 0;
+    $b = 0;
+    $c = 0;
+
+    //incremento
+
+    echo $a++;
+    echo "<br>";
+    echo $a = $a +1;
+
+    //decremento
+    
+    echo "<br>";
+    echo $b-- ;
+    echo "<br>";
+    
+    echo $c = $c - 1;
+?>
