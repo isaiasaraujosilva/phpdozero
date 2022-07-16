6d289f50106236ef52705b621bd7905f1fce3a96
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-15 22:40:16 -0300

    equal operator

---
+++ 5_op_expres/13_op_equality/index.php
@@ -0,0 +1,19 @@
+<?php
+    if(3==3){
+        echo "valor verdadeiro 1";
+    }
+    if(3==4){
+        echo "valor verdadeiro 2";
+    }
+
+    $a = 1;
+    $b = 2;
+    $c = 1;
+
+    if($a == $b){
+        echo "Valores iguais 1 <br>";
+    }
+    if($a == $c){
+        echo "Valores iguais 2 <br>";
+    }
+?>
