e90e828e6400f601be3e77f037c36f38a5f6ca3d
isaias-araujo1 <isaias-araujo1@hotmail.com>
2022-07-16 21:45:33 -0300

    block if testing

---
+++ 5_op_expres/14_task_15/index.php
@@ -0,0 +1,12 @@
+<?php
+    $a = 1;
+    $b = 1;
+    $c = 3;
+
+    if($a == $b){
+        echo "Valores iguais"."<br>";
+    }
+    if($a != $c){
+        echo "valores diferentes"."<br>";
+    }
+?>
